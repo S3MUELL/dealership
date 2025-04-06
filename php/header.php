28e92dcda5836b0889827da318042be1bae5778f
fabestsh <fabestsharra@gmail.com>
2025-04-06 13:55:54 +0200

    fixes

---
+++ php/header.php
@@ -0,0 +1,14 @@
+<section id="header">
+      <div class="logo">
+          <img  class="logo-animation" src="../img/logo.png" alt="text1">
+      </div>
+      <div class="nav">
+        <ul class="selector">
+          <a href="home.php"><li class="home">Home</li></a>
+          <a href="about.php"><li class="about">About</li></a>
+          <a href="models.php"><li class="models">Models</li></a>
+          <a href="contact.php"><li class="contact">Contact Us</li></a>
+          <a href="sign.php"><li class="sign"><img class="profile" src="./../img/pfp.png" alt="" /></li></a>
+        </ul>
+      </div>
+  </section>  
